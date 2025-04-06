bb502a6f3863175d20756af893ccb274781e5cef
Jesus SH <jesus.sanchez.herrero.0@gmail.com>
2025-04-06 22:56:55 +0200

    Add new changes

---
+++ data/german_roots_database.py
@@ -0,0 +1,7 @@
+# data base german words
+
+german_roots = {
+    "sehen" : ["ansehen", "zusehen", "gesehen"],
+    "sprechen" : ["besprechen", "Gespräch", "versprechen"],
+    "machen" : ["anmachen","vermachen", "machenhaft"]
+}
